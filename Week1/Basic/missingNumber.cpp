2ffca51e60231b233dcc26ff45e4d185ca2f6d49
Bhargavi Naik <168259289+bvnaik05@users.noreply.github.com>
2026-02-27 10:08:42 +0530

    Missing Number LC

---
+++ Week1/Basic/missingNumber.cpp
@@ -0,0 +1,9 @@
+class Solution {
+public:
+    int missingNumber(vector<int>& nums) {
+        int n = nums.size();
+        int sum = 0;
+        for(auto it: nums)  sum+= it;
+        return (n * (n + 1) / 2) - sum;
+    }
+};
