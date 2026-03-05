39f2f3269aa3f23b94a09a30e6274fe9dfd8b41e
Bhargavi Naik <168259289+bvnaik05@users.noreply.github.com>
2026-03-05 22:29:01 +0530

    Maximum Average Subarray I LC

---
+++ Week2/Basic/findMaxAverage.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    double findMaxAverage(vector<int>& nums, int k) {
+        int n = nums.size();
+        double sum = 0;
+
+        for(int i = 0; i < k; i++){
+            sum += nums[i];
+        }
+
+        double maxi = sum;
+
+        for(int i = k; i < n; i++){
+            sum += nums[i];
+            sum -= nums[i-k];
+            if(sum > maxi) maxi = sum;
+        }
+
+        return maxi / k;
+    }
+};
