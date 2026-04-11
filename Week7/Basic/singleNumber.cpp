e674405e77ce5e726a0ec714cd25978e7011e676
Bhargavi Naik <168259289+bvnaik05@users.noreply.github.com>
2026-04-11 09:46:00 +0530

    Single Number LC

---
+++ Week7/Basic/singleNumber.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int singleNumber(vector<int>& nums) {
+        int xori = 0;
+        for(auto it: nums){
+            xori = xori ^ it;
+        }
+        return xori;
+    }
+};
